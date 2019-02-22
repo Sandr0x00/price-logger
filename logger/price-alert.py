629aa0dafb6a8f2be0972fdf3a3c4cf0778f8422
Sandro Bauer <sandro.bauer.93@gmail.com>
2019-02-22 13:41:41 +0100

    Working price logger. First version.

---
+++ logger/price-alert.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import time
+from datetime import date, datetime
+import requests
+import argparse
+import logging
+from lxml import html
+from urllib.parse import urljoin
+import random
+
+def log_price(item_id: str, price):
+    current_time = datetime.now().isoformat(timespec='minutes')
+    logger.info(f'{current_time} - {item_id} - {price:.2f}')
+    with open(f'../logs/{item_id}.txt', 'a+') as file:
+        file.write(f"{current_time} - {price:.2f}\n")
+
+
+def get_price(item):
+    url = urljoin(config['base_url'], item)
+    r = requests.Session().get(url, headers={
+        'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+            '(KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+    })
+    r.raise_for_status()
+    tree = html.fromstring(r.text)
+    try:
+        # extract the price from the string
+        price_string = re.findall('\d+.\d+', tree.xpath(selector)[0].text)[0]
+        price = float(price_string.replace(',', '.'))
+        log_price(item, price)
+    except (IndexError, TypeError) as e:
+        logger.debug(e)
+        logger.warning('Didn\'t find the \'price\' element, trying again later')
+
+
+def get_config(config):
+    with open(config, 'r') as f:
+        return json.loads(f.read())
+
+
+def config_logger(debug):
+    global logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config',
+        default='%s/config.json' % os.path.dirname(os.path.realpath(__file__)),
+        help='Configuration file path')
+    parser.add_argument('-d', '--debug',
+        action='store_true',
+        help='Enable debug level logging')
+    return parser.parse_args()
+
+
+def main():
+    global config
+    global selector
+    args = parse_args()
+    config_logger(args.debug)
+    config = get_config(args.config)
+    selector = config['xpath_selector']
+
+    poll_interval = config["poll"]
+    poll_deviation = config["poll_deviation"]
+    check_timeout = config["check_timeout"]
+    logger.info(f"Configured poll intervall {poll_interval} Seconds with deviation of {poll_deviation} Seconds.")
+
+    # build intervall list { "id": "next execution" }
+    items = {}
+    for item in config['items']:
+        initial_delay = random.randint(0, poll_interval)
+        current_time = datetime.now().timestamp()
+        items[item] = current_time + initial_delay
+
+    while True:
+        current_time = datetime.now().timestamp()
+
+        for item, next_exec in items.items():
+            if next_exec < current_time:
+                get_price(item)
+                items[item] = current_time + random.randint(poll_interval - poll_deviation, poll_interval + poll_deviation)
+
+        time.sleep(check_timeout)
+
+
+if __name__ == '__main__':
+    main()
